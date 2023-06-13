7deded7eb9205cd56939aa4e8dcbe7736f5b102c
Shivansh-Jain-github <113490259+Shivansh-Jain-github@users.noreply.github.com>
2023-06-13 20:39:04 +0530

    Time: 748 ms (17.41%), Space: 66.9 MB (40.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-         unordered_map<int, bool> map;
-        for(int i = 0; i<nums.size(); i++){
-            map[nums[i]] = true;
+        // Create an unordered set to store unique elements
+        unordered_set<int> set;
+        
+        // Insert all elements from nums into the set
+        for(int val : nums){
+            set.insert(val);
         }
-        for(int i=0; i<nums.size(); i++){
-            if(map.count(nums[i]-1) > 0){
-                map[nums[i]] = false;
+        
+        // Initialize the result variable to store the longest consecutive sequence length
+        int res = 0;
+        
+        // Iterate over each element in nums
+        for(int num : nums){
+            // Check if num-1 is not present in the set,
+            // indicating the start of a new consecutive sequence
+            if(set.find(num-1) == set.end()){
+                int currnum = num;
+                int seq_indx = 1;
+                
+                // Iterate to find the length of the consecutive sequence
+                while(set.find(currnum+1) != set.end()){
+                    currnum++;
+                    seq_indx++;
+                }
+                
+                // Update the result with the maximum length encountered so far
+                res = max(res, seq_indx);
             }
         }
-        int maxlen = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(map[nums[i]] == true){
-                int j=0; int count=0;
-                while(map.count(nums[i]+j) > 0){
-                    j++;
-                    count++;
-                }
-                if(count>maxlen){
-                    maxlen = count;
-                }
-            }
-        }
-        return maxlen;
+        
+        // Return the length of the longest consecutive sequence
+        return res;
     }
-    
 };
