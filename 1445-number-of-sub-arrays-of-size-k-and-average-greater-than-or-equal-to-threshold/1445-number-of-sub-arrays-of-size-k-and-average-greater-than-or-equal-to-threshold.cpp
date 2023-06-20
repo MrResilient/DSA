9fd2224b501cb1dd05537b3731f97dbfcdf43b5c
Shivansh-Jain-github <113490259+Shivansh-Jain-github@users.noreply.github.com>
2023-06-20 10:02:11 +0530

    Time: 117 ms (7.39%), Space: 55.3 MB (93.4%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -4,24 +4,15 @@ public:
         int len = arr.size();
         int l = 0, r = 0;
         int count = 0, sum = 0;
-        // int sz = 0;
 
         while(r < len){                       
 
             sum += arr[r];
 
             if(r - l + 1 < k){
-                // sum = 0;
-                // sz = 0;
-                // l++;
                 r++;
                 continue;
             }
-            // else{
-            //     sum += arr[r];
-            //     sz++;
-            //     r++;
-            // }
             
             if((sum/k) >= threshold){
                 count++;
