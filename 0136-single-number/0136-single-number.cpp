e627e334ef9cd6f3cb8a3e45d6d75bf2cc28b12f
Shivansh-Jain-github <113490259+Shivansh-Jain-github@users.noreply.github.com>
2023-06-25 16:41:29 +0530

    Time: 24 ms (52.66%), Space: 16.9 MB (68.27%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,17 +3,9 @@ public:
     int singleNumber(vector<int>& nums) {
         int len  = nums.size();
         int ans = 0;
-        unordered_map<int, int> vec;
-        for(int i = 0; i < len; i++){
-            vec[nums[i]]++;
-        }
-
-        for(int i=0; i < len; ++i){
-            if(vec[nums[i]] == 1){
-                ans = nums[i];
-                break;
-            }
-        }
+        for(auto it : nums)
+            ans ^= it;
+            
         return ans;
     }
 };
