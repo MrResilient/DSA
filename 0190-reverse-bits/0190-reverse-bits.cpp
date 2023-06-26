d5b3d06642e0b6a429d90e651bc32c02a5d4673a
Shivansh-Jain-github <113490259+Shivansh-Jain-github@users.noreply.github.com>
2023-06-26 12:25:24 +0530

    Time: 3 ms (53.66%), Space: 5.9 MB (96.53%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        bitset<32> bi(n);
-        string str = bi.to_string();
-        reverse(str.begin(), str.end());
-        bitset<32> b2(str);
-        return b2.to_ullong();
+        
+        uint32_t res = 0;
+
+        for(int i = 0; i < 32; ++i){
+            int bit = (n>>i) & 1;
+            res = res | (bit<<(31 - i));
+        }
+
+        return res;
     }
 };
