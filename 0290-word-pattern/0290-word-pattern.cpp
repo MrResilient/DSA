5284484f1807c16b86281ea55e965505564e8328
Shivansh-Jain-github <113490259+Shivansh-Jain-github@users.noreply.github.com>
2023-08-31 19:27:42 +0530

    Time: 3 ms (41.93%), Space: 6.3 MB (88.3%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,34 +1,67 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-          unordered_map<char, string> charToStrMap;
-        unordered_map<string, char> strToCharMap;
-        vector<string> words;
+        
+        unordered_map<char, string> cts;
+        unordered_map<string, char> stc;
 
-        // Tokenize the string 's' into words
+        vector<string> extract;
+
         istringstream iss(s);
-        string word;
-        while (iss >> word) {
-            words.push_back(word);
+        string wrd;
+        while(iss >> wrd){
+            extract.push_back(wrd);
         }
-
-        if (pattern.length() != words.size()) {
+        if(pattern.size() != extract.size()){
             return false;
         }
 
         int len = pattern.length();
-        for (int i = 0; i < len; ++i) {
-            char c = pattern[i];
-            string word = words[i];
 
-            if (charToStrMap.find(c) == charToStrMap.end() && strToCharMap.find(word) == strToCharMap.end()) {
-                charToStrMap[c] = word;
-                strToCharMap[word] = c;
-            } else if (charToStrMap[c] != word || strToCharMap[word] != c) {
+        for(int i = 0; i < len; ++i){
+            char c = pattern[i];
+            string w = extract[i];
+
+            if(cts.find(c) == cts.end() && stc.find(w) == stc.end()){
+                cts[c] = w;
+                stc[w] = c;
+            } else if (cts[c] != w || stc[w] != c){
                 return false;
             }
         }
-
         return true;
     }
 };
+
+
+
+
+// unordered_map<char, string> charToStrMap;
+//         unordered_map<string, char> strToCharMap;
+//         vector<string> words;
+
+//         // Tokenize the string 's' into words
+//         istringstream iss(s);
+//         string word;
+//         while (iss >> word) {
+//             words.push_back(word);
+//         }
+
+//         if (pattern.length() != words.size()) {
+//             return false;
+//         }
+
+//         int len = pattern.length();
+//         for (int i = 0; i < len; ++i) {
+//             char c = pattern[i];
+//             string word = words[i];
+
+//             if (charToStrMap.find(c) == charToStrMap.end() && strToCharMap.find(word) == strToCharMap.end()) {
+//                 charToStrMap[c] = word;
+//                 strToCharMap[word] = c;
+//             } else if (charToStrMap[c] != word || strToCharMap[word] != c) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
